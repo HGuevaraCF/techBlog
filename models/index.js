cb7dfc7fc842cf29d60628ecf32a8e98d17941d6
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-06-18 00:09:29 -0500

    rules

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsToMany(User, {
+    foreignKey: 'user_id',
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
